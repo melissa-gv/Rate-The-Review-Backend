9816301d63e42fb8b9c251a7eb00d6def26bf19c
melissa-gv <melissagilv@gmail.com>
2022-11-30 16:29:53 -0600

    In progress refactor from promise chainging to async await

--- server/controllers/reviewsCntl.js
+++ server/controllers/reviewsCntl.js
@@ -9,17 +9,26 @@ const YELP_URL = 'https://api.yelp.com/v3'
 const { API_KEY } = process.env
 
 const getBusinesses = (req, res) => {
+  const offsetNum = Math.floor(Math.random() * 1000)
+  console.log('offsetNum:', offsetNum)
   axios.get(`${YELP_URL}/businesses/search`, {
     headers: { Authorization: `${API_KEY}` },
     params: {
       term: 'restaurants',
       location: req.query.location,
       radius: 10000,
-      limit: 3,
+      limit: 6,
+      offset: offsetNum,
     },
   })
     .then((businessesResponse) => {
       getReviews(req, res, businessesResponse.data.businesses)
+      console.log('0 Business Name:', businessesResponse.data.businesses[0].name)
+      console.log('1 Business Name:', businessesResponse.data.businesses[1].name)
+      console.log('2 Business Name:', businessesResponse.data.businesses[2].name)
+      console.log('3 Business Name:', businessesResponse.data.businesses[3].name)
+      console.log('4 Business Name:', businessesResponse.data.businesses[4].name)
+      console.log('5 Business Name:', businessesResponse.data.businesses[5].name)
     })
     .catch((err) => {
       console.log('err', err)
@@ -27,19 +36,25 @@ const getBusinesses = (req, res) => {
 }
 
 const getReviews = (req, res, businesses) => {
-  const reviews = {}
+  const reviews = []
 
-  Promise.all(businesses.map((business, index) => axios.get(business.url, {
+  Promise.allSettled(businesses.map((business) => axios.get(business.url, {
     headers: { Authorization: `${API_KEY}` },
   })
     .then((reviewsResponse) => {
       const dom = new JSDOM(reviewsResponse.data)
-      const rating = Number(dom.window.document.getElementById('reviews').querySelector('.five-stars--regular__09f24__DgBNj').attributes['aria-label'].textContent.substring(0, 1))
-      const reviewText = dom.window.document.getElementById('reviews').querySelector('.comment__09f24__gu0rG').textContent
-      reviews[index] = { rating, reviewText }
+      const rating1 = Number(dom.window.document.getElementById('reviews').querySelectorAll('.five-stars--regular__09f24__DgBNj')[0].attributes['aria-label'].textContent.substring(0, 1))
+      const rating2 = Number(dom.window.document.getElementById('reviews').querySelectorAll('.five-stars--regular__09f24__DgBNj')[1].attributes['aria-label'].textContent.substring(0, 1))
+      const reviewText1 = dom.window.document.getElementById('reviews').querySelectorAll('.comment__09f24__gu0rG')[0].textContent
+      const reviewText2 = dom.window.document.getElementById('reviews').querySelectorAll('.comment__09f24__gu0rG')[1].textContent
+      reviews.push(
+        { rating: rating1, reviewText: reviewText1 },
+        { rating: rating2, reviewText: reviewText2 },
+      )
+      console.log('reviews.length:', reviews.length)
     })))
     .then(() => {
-      console.log('data to be sent:', { reviews, businesses })
+      console.log('FINAL DATA TO BE SENT:', reviews, businesses)
       res.status(200).send({ reviews, businesses })
     })
     .catch((err) => {
